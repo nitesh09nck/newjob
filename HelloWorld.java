658a294ff9688f1a48dbe57c34bfee5b47a24528
nitesh09nck <nitesh09.nck@gmail.com>
2022-02-16 22:21:30 +0530

    newjob

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+	
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+		System.out.println("its devops class");
+		System.out.println("first java program");
+		System.out.println("this is Nitesh");
+		System.out.println("java codes");
+    }
+}
